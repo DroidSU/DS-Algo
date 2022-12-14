2255a7b643e0451698bf47bee4fcdd857e07c537
DroidSu <sujoydatta26@gmail.com>
2022-12-14 20:28:36 +0530

    Added program to linear search in array

---
+++ Data Structures/Arrays & Strings/LinearSearchArray.java
@@ -0,0 +1,20 @@
+public class LinearSearchArray {
+
+    public static void main(String args[]){
+        // input
+        int arr[] = {1,2,3,4,5,6};
+
+        // testcase
+        int n = 8;
+
+        
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == n){
+                System.out.println("Found in index: " + i);
+                return;
+            }
+        }
+
+        System.out.println("Not found");
+    }
+}
---
+++ Data Structures/Arrays & Strings/Readme.md
@@ -0,0 +1,1 @@
+This repository will be used to track my progress on DS Algo
